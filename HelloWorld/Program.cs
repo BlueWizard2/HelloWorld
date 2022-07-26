7f2d29004c0d8d33ea798833121aa8039bdcf019
BlueWizard2 <110004040+BlueWizard2@users.noreply.github.com>
2022-07-25 21:27:52 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,251 @@
+﻿Console.Title = "Morse Code Bot (Alpha)";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I am Morse Code Bot");
+Console.WriteLine("*I'm a new programer, this is my first project. Don't mind the bugs*");
+Console.WriteLine("I will display a letter");
+Console.WriteLine("Then I will play a sound that sound is the previosly displayed letter");
+
+Console.WriteLine("press [ENTER] to start");
+Thread.Sleep(5000);
+Task.Delay(5000);
+Console.WriteLine("JK I will start in 5 seconds");
+
+Thread.Sleep(5000);
+Task.Delay(5000);
+
+int L = 1000;
+int S = L / 3;
+int F = 590;
+
+Console.Clear();
+
+Console.WriteLine("A");
+Console.Beep(F, S);
+Console.Beep(F, L);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+
+Console.WriteLine("B");
+
+Console.Beep(F, L);
+Console.Beep(F, S);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+
+Console.WriteLine("C");
+
+Console.Beep(F, L);
+Console.Beep(F, S);
+Console.Beep(F, L);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+
+Console.WriteLine("D");
+
+Console.Beep(F, L);
+Console.Beep(F, S);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+
+Console.WriteLine("E");
+
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("F");
+
+Console.Beep(F, S);
+Console.Beep(F, S);
+Console.Beep(F, L);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("G");
+
+Console.Beep(F, L);
+Console.Beep(F, L);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("H");
+
+Console.Beep(F, S);
+Console.Beep(F, S);
+Console.Beep(F, S);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+
+Console.Clear();
+Console.WriteLine("I");
+
+Console.Beep(F, S);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("Words are hard.");
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+
+
+Console.WriteLine("J");
+
+Console.Beep(F, S);
+Console.Beep(F, L);
+Console.Beep(F, L);
+Console.Beep(F, L);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("K");
+
+Console.Beep(F, L);
+Console.Beep(F, S);
+Console.Beep(F, L);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("L");
+
+Console.Beep(F, S);
+Console.Beep(F, L);
+Console.Beep(F, S);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("You are an idiot");
+Console.WriteLine("LOL Your not");
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+Console.WriteLine("M");
+
+Console.Beep(590, 1000);
+Console.Beep(590, 1000);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+Console.WriteLine("We are half way there now" +
+    " I acctualy dont know what a 'half' is, my progamer is not that BIG BRAIN yet");
+Thread.Sleep(3000);
+Task.Delay(3000);
+
+Console.WriteLine("N");
+
+Console.Beep(590, 1000);
+Console.Beep(590, 500);
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.Clear();
+
+
+Console.WriteLine("..........");
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.WriteLine("Im bored, Are you?");
+Thread.Sleep(1000);
+Task.Delay(1000);
+Console.Clear();
+Console.WriteLine("i like trains");
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.WriteLine("I dont know what a train is");
+Thread.Sleep(2000);
+Task.Delay(2000);
+
+Console.WriteLine("I was just told to put that there");
+Thread.Sleep(2000);
+Task.Delay(2000);
+Console.Clear();
+
+
+Console.WriteLine("O");
+
+Console.Beep(F, L);
+Console.Beep(F, L);
+Console.Beep(F, L);
+Thread.Sleep(2000);
+Task.Delay(2000);
+Console.Clear();
+
+Console.WriteLine("P");
+Console.Beep(F, S);
+Console.Beep(F, L);
+Console.Beep(F, L);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+Console.Clear();
+
+Console.WriteLine("Q");
+Console.Beep(F, L);
+Console.Beep(F, L);
+Console.Beep(F, S);
+Console.Beep(F, L);
+Thread.Sleep(2000);
+Task.Delay(2000);
+Console.Clear();
+
+
+Console.WriteLine("R");
+Console.Beep(F, S);
+Console.Beep(F, L);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+Console.Clear();
+
+
+Console.WriteLine("S");
+Console.Beep(F, S);
+Console.Beep(F, S);
+Console.Beep(F, S);
+Thread.Sleep(2000);
+Task.Delay(2000);
+Console.Clear();
